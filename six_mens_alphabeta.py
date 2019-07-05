c31e4311ee2ee2492d6cfc18fae0303da3ab4c28
Angelica Munyao <munyaoangelica@yahoo.com>
2019-07-04 22:11:34 -0400

    Update six_mens_alphabeta.py

--- six_mens_alphabeta.py
+++ six_mens_alphabeta.py
@@ -1,7 +1,7 @@
 # Test of the alpha-beta mini-max agents with depth limiting on Six Men's Morris board
 
-from project1.alphabeta6 import MaxPlayer, MinPlayer
-from project1.six_men_morris import SixMensMorris
+from alphabeta6 import MaxPlayer, MinPlayer
+from six_men_morris import SixMensMorris
 max_player = MaxPlayer()
 min_player = MinPlayer()
 
